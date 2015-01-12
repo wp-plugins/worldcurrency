9ead5ddffe392a8587501f58e229eda38a4e784e
jonscaife <jonscaife@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 16:57:14 +0000

    Corrected typo on admin page.  Shortcode should be curr= not cur=
    git-svn-id: https://plugins.svn.wordpress.org/worldcurrency/tags/1.17@1066191 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- worldcurrency-admin.php
+++ worldcurrency-admin.php
@@ -10,30 +10,30 @@
 
 		// get the localcurrency options
 		$dt_wc_options = get_option('dt_wc_options');
-		
+
 		// Post the form
 		if (isset($_POST['dt_wc_options_submit']) && check_admin_referer('worldcurrency_admin_page_submit')) {
 
-			$dt_wc_options['historic_rates']		= isset($_POST['historic_rates']) ? 'true' : 'false'; 
-			$dt_wc_options['hide_if_same']			= isset($_POST['hide_if_same']) ? 'true' : 'false'; 
-			$dt_wc_options['plugin_link']			= isset($_POST['plugin_link']) ? 'true' : 'false'; 
-			$dt_wc_options['yahoo_link']			= isset($_POST['yahoo_link']) ? 'true' : 'false'; 
-			$dt_wc_options['plugin_priority'] 		= absint($_POST['plugin_priority']); 
-			$dt_wc_options['output_format'] 		= $_POST['output_format']; 
-			$dt_wc_options['thousands_separator'] 	= $_POST['thousands_separator']; 
-			$dt_wc_options['decimal_separator'] 	= $_POST['decimal_separator']; 
-			$dt_wc_options['additional_css'] 		= $_POST['additional_css']; 
-			$dt_wc_options['bottom_select']			= isset($_POST['bottom_select']) ? 'true' : 'false'; 
-			$dt_wc_options['include_jquery']		= isset($_POST['include_jquery']) ? 'true' : 'false'; 
-			$dt_wc_options['include_always']		= isset($_POST['include_always']) ? 'true' : 'false'; 
-			$dt_wc_options['jquery_no_conflict']	= isset($_POST['jquery_no_conflict']) ? 'true' : 'false'; 
-			$dt_wc_options['ajax_over_ssl']			= isset($_POST['ajax_over_ssl']) ? 'true' : 'false'; 
-			
+			$dt_wc_options['historic_rates']		= isset($_POST['historic_rates']) ? 'true' : 'false';
+			$dt_wc_options['hide_if_same']			= isset($_POST['hide_if_same']) ? 'true' : 'false';
+			$dt_wc_options['plugin_link']			= isset($_POST['plugin_link']) ? 'true' : 'false';
+			$dt_wc_options['yahoo_link']			= isset($_POST['yahoo_link']) ? 'true' : 'false';
+			$dt_wc_options['plugin_priority'] 		= absint($_POST['plugin_priority']);
+			$dt_wc_options['output_format'] 		= $_POST['output_format'];
+			$dt_wc_options['thousands_separator'] 	= $_POST['thousands_separator'];
+			$dt_wc_options['decimal_separator'] 	= $_POST['decimal_separator'];
+			$dt_wc_options['additional_css'] 		= $_POST['additional_css'];
+			$dt_wc_options['bottom_select']			= isset($_POST['bottom_select']) ? 'true' : 'false';
+			$dt_wc_options['include_jquery']		= isset($_POST['include_jquery']) ? 'true' : 'false';
+			$dt_wc_options['include_always']		= isset($_POST['include_always']) ? 'true' : 'false';
+			$dt_wc_options['jquery_no_conflict']	= isset($_POST['jquery_no_conflict']) ? 'true' : 'false';
+			$dt_wc_options['ajax_over_ssl']			= isset($_POST['ajax_over_ssl']) ? 'true' : 'false';
+
 			update_option('dt_wc_options', $dt_wc_options);
-			
+
 			echo '<div id="message" class="updated"><p><strong>Options saved.</strong></p></div>';
 		}
-		 
+
 		if (isset($_POST['dt_wc_options_reset']) && check_admin_referer('worldcurrency_admin_page_submit')) {
 			dt_wc_createOptions(true);
 			echo '<div id="message" class="updated"><p><strong>Options resetted</strong></p></div>';
@@ -42,7 +42,7 @@
 
 		// now drop out of php to create the HTML for the Options page
 	?>
-	
+
 		<style type="text/css">
 			.dt_wc_list {
 				list-style-type: disc;
@@ -51,18 +51,18 @@
 			}
 		</style>
 
-		<div class="wrap"> 
-			<h2>WorldCurrency Settings</h2>			
+		<div class="wrap">
+			<h2>WorldCurrency Settings</h2>
 			<div id="poststuff">
-			
-				<form name="worldcurrency_options_form" action="" method="post">			
-				
+
+				<form name="worldcurrency_options_form" action="" method="post">
+
 				<div class="stuffbox">
 					<h3>General Settings</h3>
 					<div class="inside">
-	
+
 						<?php if (function_exists('wp_nonce_field')) {wp_nonce_field('worldcurrency_admin_page_submit'); }?>
-					
+
 						<p>
 							<h4>
 								Historyc Exchange Rates:
@@ -72,7 +72,7 @@
 							Select this parameter if you want the use the exchange rate at the time of the post, rather than the current one.<br />
 							This default option can be overridden by a parameter directly in the shorttag.
 						</p>
-					
+
 						<p>
 							<h4>
 								Hide conversion if same currency as origin value:
@@ -108,7 +108,7 @@
 							</h4>
 							The character used to separate the tousands in the output (usually , or .)
 						</p>
-						
+
 						<p>
 							<h4>
 								Decimals separator:
@@ -116,7 +116,7 @@
 							</h4>
 							The character used to separate the decimal numbers in the output (usually , or .)
 						</p>
-						
+
 						<p>
 							<h4>
 								Bottom currency select box:
@@ -124,7 +124,7 @@
 							</h4>
 							Automatically puts a currency selection box at the bottom of every page/post that has currency conversions in them.
 						</p>
-						
+
 						<p>
 							<h4>
 								Plugin Link:
@@ -133,7 +133,7 @@
 							By default, a link to the plugin home page is included near the currency selection box.<br />
 							Deselect this parameter if you want to disable the link, but <u>keep in mind that leaving the link is one the best ways to support this plugin</u>.
 						</p>
-						
+
 						<p>
 							<h4>
 								Yahoo Link:
@@ -142,7 +142,7 @@
 							By legal requirement a link to Yahoo! Finance will be shown near the currency selection box.<br />
 							Disable this only if you want to put this link elsewhere (ie: footer).
 						</p>
-												
+
 						<p>
 							<h4>
 								Additional CSS styles:<br />
@@ -168,7 +168,7 @@
 							</h4>
 							This plugin uses and includes jQuery, if you already have jQuery included in your blog pages (by theme or other plugins), you may want to disable this.
 						</p>
-						
+
 						<p>
 							<h4>
 								jQuery no conflict:
@@ -176,7 +176,7 @@
 							</h4>
 							Uses "jQuery" instead of "$" for calling jQuery functions. Usefull also if you have a noconflict version of jQuery included by other themes/plugins
 						</p>
-						
+
 						<p>
 							<h4>
 								Always include the script:
@@ -184,7 +184,7 @@
 							</h4>
 							Always include the script in the page. Usefull if you want to use the currency conversions also in other parts than pages/posts. (in pages/posts you can force the script presence with a wc_force=1 custom fiels)
 						</p>
-						
+
 						<p>
 							<h4>
 								Ajax over SSL:
@@ -192,7 +192,7 @@
 							</h4>
 							Flag this if your site runs over SSL and you want worldcurrency ajax queries run over SSL too.
 						</p>
-						
+
 						<p>
 							<h4>
 								Plugin Firing Priorty:
@@ -202,27 +202,27 @@
 							10 is the default priority, change the value (probably to higher values) only if you have
 							some problems.
 						</p>
-											
-					</div> 
-				</div> 
-				
+
+					</div>
+				</div>
+
 				<!-- Show Update Button -->
 				<div class="submit">
 					<input type="submit" name="dt_wc_options_submit" value="Update Options &raquo;"/>
 					<input type="submit" name="dt_wc_options_reset" value="Reset Options &raquo;"/>
-				</div>	
+				</div>
 
 				</form>
-				
+
 				<!-- Usage Section -->
 				<div class="stuffbox">
 					<h3>Usage</h3>
 					<div class="inside">
 						<p>
-							Enter any currency values you want converted with the [worldcurrency] shorttag.<br /> <br /> 
+							Enter any currency values you want converted with the [worldcurrency] shorttag.<br /> <br />
 							<code style="padding:5px;margin:10px;">
-								[worldcurrency cur="EUR" value="25"]
-							</code><br /><br /> 
+								[worldcurrency curr="EUR" value="25"]
+							</code><br /><br />
 							in united states this will show: (~30$ USD)
 						</p>
 						<p>
@@ -245,8 +245,8 @@
 							wherever you want the <b>currency selection box</b> to be shown (or select the option above to show it automatically at the bottom of the post)<br />
 							<br />
 							There is also a <b>Widget</b> (for the sidebar) wich shows the Currency Selection Box only if there are currencies on the page.
-					</div> 
-				</div> 
+					</div>
+				</div>
 
 
 				<!-- Thank You Section -->
@@ -261,14 +261,14 @@
 							<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
 							</form>
 						</div>
-						
+
 						<p>Thank you for using the WorldCurrency plugin. If you like this plugin, you may like to:</p>
 						<ul style="font-size:1em; margin:12px 40px; list-style:disc">
 							<li>Rate this plugin on the <a href="http://wordpress.org/extend/plugins/worldcurrency/" target="_blank">WordPress plugin directory</a></li>
 							<li>Link to the <a href="http://www.cometicucinoilweb.it/blog/en/worldcurrency/" target="_blank">plugin's home page</a> so others can find out about it</li>
 							<li>Support ongoing development by <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6CUNDYFWV4NUW">making a donation</a></li>
 						</ul>
-						
+
 						<p>This plugin was created thanks to:</p>
 						<ul style="font-size:1em; margin:12px 40px; list-style:disc">
 							<li>Many ideas and some code from <a href="http://www.jobsinchina.com/resources/wordpress-plugin-localcurrency" target="_blank">LocalCurrency</a> by Stephen Cronin</li>
@@ -276,8 +276,8 @@
 							<li>Yahoo! Finance (<a href="http://finance.yahoo.com" target="_blank">http://finance.yahoo.com</a>) for conversion rates</li>
 						</ul>
 						<p><small>Copyright <?php echo date('Y'); ?> by Daniele Tieghi. Released under the GNU General Public License (version 2 or later).</small></p>
-					</div> 
-				</div> 
+					</div>
+				</div>
 
 			</div> <!-- end id="poststuff" -->
 		</div> <!-- end class="wrap" -->
